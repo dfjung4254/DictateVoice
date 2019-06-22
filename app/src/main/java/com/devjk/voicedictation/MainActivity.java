01e81910e8737a604dd3e8f3cae95856849afd74
dfjung4254 <rmsghk4254@naver.com>
2019-06-22 22:22:05 +0900

    change custom UI

--- app/src/main/java/com/devjk/voicedictation/MainActivity.java
+++ app/src/main/java/com/devjk/voicedictation/MainActivity.java
@@ -9,7 +9,9 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.service.autofill.TextValueSanitizer;
+import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -22,6 +24,12 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView txtDictate;
     private ImageView imgvIcon;
+    private TextView txtIcon;
+
+    private boolean isDictating;
+
+    private SpeechRecognizer mRecognizer;
+    private Intent intent;
 
     final int RESULT_SPEECH_RECOGNIZE = 1000;
 
@@ -32,26 +40,54 @@ public class MainActivity extends AppCompatActivity {
 
         /* 초기화 */
         txtDictate = findViewById(R.id.MainActivity_TextView_dictate);
-        imgvIcon = findViewById(R.id.MainActivity_ImageView_icon);
+        imgvIcon = findViewById(R.id.MainActivity_ImageView_Mic);
+        txtIcon = findViewById(R.id.MainActivity_TextView_Mic);
+        isDictating = false;
 
+        /* Onclick Listener */
         imgvIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                speak();
+                if(!isDictating){
+                    /* 음성인식 재생 아닌 상태 - 시작 */
+                    speak();
+                }else{
+                    /* 음성인식 재생 중인 상태 - 중단 */
+                    stopSpeak();
+                }
+
             }
         });
     }
 
+    /* 음성인식 중단 */
+    private void stopSpeak(){
+        mRecognizer.stopListening();
+        isDictating = false;
+
+        txtIcon.setText(R.string.touchStart);
+        imgvIcon.setImageResource(R.drawable.mic_black);
+    }
+
+    /* 음성인식 시작 */
     private void speak(){
 
         try{
             /* 성공 시 스피치 다이얼로그를 보여준다. */
-            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
             intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                     RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
             intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hey Speak Something");
-            startActivityForResult(intent, RESULT_SPEECH_RECOGNIZE);
+            intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
+
+            mRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+            mRecognizer.setRecognitionListener(listener);
+            mRecognizer.startListening(intent);
+            isDictating = true;
+
+            txtIcon.setText(R.string.touchFinish);
+            imgvIcon.setImageResource(R.drawable.mic_pink);
+
         }catch (ActivityNotFoundException e){
             /* 실패 - 구글 관련 ACTION_RECOGNIZE_SPEECH 를 설치해주어야 함.*/
             String appPackageName = "com.google.android.googlequicksearchbox";
@@ -64,18 +100,70 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    private RecognitionListener listener = new RecognitionListener() {
+        @Override
+        public void onReadyForSpeech(Bundle bundle) {
 
-        switch (requestCode){
-            case RESULT_SPEECH_RECOGNIZE:{
-                if(resultCode == RESULT_OK && data != null){
-                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    txtDictate.setText(result.get(0));
-                }
-            }
         }
 
-    }
+        @Override
+        public void onBeginningOfSpeech() {
+
+        }
+
+        @Override
+        public void onRmsChanged(float v) {
+
+        }
+
+        @Override
+        public void onBufferReceived(byte[] bytes) {
+
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+
+        }
+
+        @Override
+        public void onError(int i) {
+
+        }
+
+        @Override
+        public void onResults(Bundle bundle) {
+            String key = "";
+            key = SpeechRecognizer.RESULTS_RECOGNITION;
+            ArrayList<String> mResult = bundle.getStringArrayList(key);
+            String[] rs = new String[mResult.size()];
+            mResult.toArray(rs);
+            txtDictate.setText(mResult.get(0));
+        }
+
+        @Override
+        public void onPartialResults(Bundle bundle) {
+
+        }
+
+        @Override
+        public void onEvent(int i, Bundle bundle) {
+
+        }
+    };
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        switch (requestCode){
+//            case RESULT_SPEECH_RECOGNIZE:{
+//                if(resultCode == RESULT_OK && data != null){
+//                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+//                    txtDictate.setText(result.get(0));
+//                }
+//            }
+//        }
+//
+//    }
 }
